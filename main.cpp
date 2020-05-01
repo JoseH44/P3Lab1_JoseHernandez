dc456cbe644786a43a84986c6b3dc41af10e2560
Jose <jrdjhc45@gmail.com>
2020-05-01 14:10:20 -0600

    validacion del numero menor a 100

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int num;
+	cout<<"Ingrese un Numero menor que 100: ";
+	cin>>num;
+	if(num < 100){
+		
+	}else{
+		cout<<"El Numero Debe ser Menor que 100";
+	}
+	
+	
+	return 0;
+}
